a4fa84f5bd3658415218d67bcf0e01682665777c
John Verzani <jverzani@gmail.com>
2012-04-09 21:16:59 -0400

    modify test

--- inst/tests/test-gedit.R
+++ inst/tests/test-gedit.R
@@ -1,7 +1,8 @@
 test.gedit <- function() {
   w <- gwindow()
   g <- ggroup(cont = w, horiz = FALSE)
-
+  gbutton("Focus here", cont=g)
+  
   text <- "label text"; newText <- "new"
   l <- gedit(text, cont = g)
 
@@ -12,7 +13,9 @@ test.gedit <- function() {
   svalue(l) <- newText
   checkEquals(svalue(l), newText)
 
+  l2 <- gedit("", initial="is this initial text? State name", cont=g)
+  checkEquals(svalue(l2), "")
   # [<-
-  l[] <- state.name
+  l2[] <- state.name
   
 }
