dff0e156746004ac29e023932ed37399dd96ba68
John Verzani <jverzani@gmail.com>
2012-04-24 22:57:00 -0400

    move timer out of code, makes this toolkit independent.

--- R/ws-model.R
+++ R/ws-model.R
@@ -2,18 +2,19 @@
 NULL
 
 
-##' A class for monitoring workspacke
+##' A class for monitoring workspace changes
 ##'
-##' A reference class to create a model that monitors the global workspace. The class has
-##' methods \code{update_state}, \code{start_timer}, \code{stop_timer} and the "getting" methods
-##' \code{get_by_class}, \code{get_by_function} (filter), \code{get_changes}
+##' A reference class to create a model that monitors the global
+##' workspace. The class has method \code{update_state} and the
+##' "getting" methods \code{get_by_class}, \code{get_by_function}
+##' (filter), \code{get_changes}. Use with a \code{gtimer} instance to
+##' keep up to date with changes to the workspace.
 ##' @exportClass WSWatcherModel
 ##' @rdname S4-classes
 ##' @name WSWatcherModel-class
 WSWatcherModel <-  setRefClass("WSWatcherModel",
                                contains="Observable",
                                fields=list(
-                                 timer="ANY",
                                  nms="character",
                                  digests="ANY",
                                  old_nms = "character",
@@ -21,31 +22,17 @@ WSWatcherModel <-  setRefClass("WSWatcherModel",
                                  changes = "list" # should signal when updated
                                  ),
                                methods=list(
-                                 initialize=function(toolkit=NULL, ...) {
+                                 initialize=function( ...) {
                                    "Initialze state of cached objects"
 
-                                   timer <<- gtimer(ms=1000, FUN=function(ws_model) {
-                                     ws_model$update_state()
-                                   }, data=.self, start=FALSE, toolkit=toolkit)
                                    
                                    update_state() # initial
                                    old_nms <<- nms
                                    old_digests <<- digests
                                    callSuper(...)
                                  },
-                                 update_timer=function(n) {
-                                   "Adjust timer to size of workspace"
-                                   if(timer$started) {
-                                     cur <- timer$interval
-                                     suggested <- floor(log(1 + n)) * 1000 # XXX too long?
-                                     if(cur != suggested) {
-                                       stop_timer()
-                                       start_timer(suggested)
-                                     }
-                                   }
-                                 },
                                  update_state=function(...) {
-                                   "update cache of names/digests"
+                                   "update cache of names/digests, then notify observers if there are changes"
                                    nms <<- ls(envir=.GlobalEnv)
                                    ## avoid certain classes
                                    skip_these <- function(x) !(is(x, "DigestClass") || is(x, "envRefClass"))
@@ -59,9 +46,9 @@ WSWatcherModel <-  setRefClass("WSWatcherModel",
                                                       )
                                      old_nms <<- nms
                                      old_digests <<- digests
+                                     ## notify any observers if there are any changes
                                      notify_observers()
                                    }
-                                   update_timer(length(nms))
                                  },
                                  any_changes=function(...) {
                                    "Report  if any changes"
@@ -71,15 +58,6 @@ WSWatcherModel <-  setRefClass("WSWatcherModel",
                                      out <- (length(old_digests) != length(digests)) || any(old_digests != digests)
                                    }
                                    out
-                                 },
-                                 start_timer=function(ms) {
-                                   "Start timer. Can modify ms here"
-                                   if(!missing(ms)) 
-                                     timer$set_interval(ms)
-                                   timer$start_timer()
-                                 },
-                                 stop_timer=function() {
-                                   timer$stop_timer()
                                  },
                                  ## get
                                  get_by_class = function(classes=character(0)) {
@@ -91,11 +69,20 @@ WSWatcherModel <-  setRefClass("WSWatcherModel",
                                  },
                                  get_by_function= function(f) {
                                    "Filter objects by function f"
+                                   
                                    objs <- mget(nms, .GlobalEnv, ifnotfound=list(function(x) {}))
                                    Filter(f, objs)
                                  },
                                  get_changes=function() {
                                    "Return list of changes"
                                    changes
+                                 },
+                                 pop_changes= function() {
+                                   "pop changes, reset"
+                                   x <- get_changes()
+                                   changes <<- list(removed=character(0),
+                                                    added=character(0),
+                                                    changed=character(0))
+                                   x
                                  }
                                  ))
