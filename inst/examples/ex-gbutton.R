39ded1a742c262b33fbdeffa3e36799efd8064e7
jverzani <jverzani@gmail.com>
2014-07-27 16:50:56 -0400

    run document, add example to gbutton

--- inst/examples/ex-gbutton.R
+++ inst/examples/ex-gbutton.R
@@ -23,5 +23,17 @@ if(interactive()) {
       svalue(b2) <- "open"
   })
 
+
+  ## handlers can be blocked/unblocked
+  b5 <- gbutton("Click me for a message", cont=g)
+  id <- addHandlerClicked(b5, function(h,...) print("Ouch"))
+  b6 <- gcheckbox("toggle handler message", cont=g, use.togglebutton=TRUE, handler=function(h,...) {
+      if (svalue(b6)) {
+          blockHandler(b5, id)
+      } else {
+          unblockHandler(b5, id)
+      }
+  })
+  
   visible(w) <- TRUE
 }
